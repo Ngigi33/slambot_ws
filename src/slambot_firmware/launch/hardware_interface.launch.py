7791738292a9380c1f422a056dded60dd23576e6
Ngigi33 <albertngigi3@gmail.com>
2024-11-15 18:20:52 +0300

    Commit

--- src/slambot_firmware/launch/hardware_interface.launch.py
+++ src/slambot_firmware/launch/hardware_interface.launch.py
@@ -47,7 +47,7 @@ def generate_launch_description():
             os.path.join(
                 get_package_share_directory("slambot_controller"),
                 "config",
-                "slambot_controllers.yaml",
+                "pentaslam_controllers.yaml",
             ),
         ],
     )
