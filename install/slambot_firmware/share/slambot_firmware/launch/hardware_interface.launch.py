58f42d2fb6e7852ac28de5d5104811e607029a00
Ngigi33 <albertngigi3@gmail.com>
2024-11-15 17:56:54 +0300

    Commit

---
+++ install/slambot_firmware/share/slambot_firmware/launch/hardware_interface.launch.py
@@ -0,0 +1,60 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import (
+    DeclareLaunchArgument,
+    SetEnvironmentVariable,
+    IncludeLaunchDescription,
+)
+import os
+from ament_index_python.packages import get_package_share_directory, get_package_prefix
+from launch_ros.parameter_descriptions import ParameterValue
+from launch.substitutions import Command, LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+
+def generate_launch_description():
+
+    robot_description = ParameterValue(
+        Command(
+            [
+                "xacro ",
+                os.path.join(
+                    get_package_share_directory("slambot_description"),
+                    "urdf",
+                    "pentaslam.urdf.xacro",
+                ),
+                " is_sim:=False",
+            ]
+        ),
+        value_type=str,
+    )
+
+    robot_state_publisher = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[
+            {
+                "robot_description":robot_description,
+            }
+        ],
+    )
+
+    controller_manager = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[
+            {"robot_description":robot_description, "use_sim_time": False},
+            os.path.join(
+                get_package_share_directory("slambot_controller"),
+                "config",
+                "slambot_controllers.yaml",
+            ),
+        ],
+    )
+
+    return LaunchDescription(
+        [
+            robot_state_publisher,
+            controller_manager,
+        ]
+    )
