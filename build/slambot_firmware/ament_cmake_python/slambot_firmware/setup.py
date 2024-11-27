b41ea29934aee67f3750ddcd993702e482fe3b70
Einstein <101053582+Ngigi33@users.noreply.github.com>
2024-11-27 23:00:49 +0300

    Delete build directory

--- build/slambot_firmware/ament_cmake_python/slambot_firmware/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages
-from setuptools import setup
-
-setup(
-    name='slambot_firmware',
-    version='0.0.0',
-    packages=find_packages(
-        include=('slambot_firmware', 'slambot_firmware.*')),
-)
